734e7e06b6f0468892c6f4f8634767f5a8a07205
InkasRas <petrov.andreha@gmail.com>
2020-03-29 00:21:13 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,20 @@
-from requests import get, post, delete, patch
+from requests import get, post, patch
 import json
-import datetime
 
-# ID не существует
-print(patch('http://127.0.0.1:5000/api/jobs/500', json={'collaborators': '1, 4, 3'}).json())
-
-# Нет такой колонки в таблице(bad_attr)
-print(patch('http://127.0.0.1:5000/api/jobs/13', json={'bad_attr': 'No', 'collaborators': '1, 4, 3'}).json())
-
-print(patch('http://127.0.0.1:5000/api/jobs/13', json={'collaborators': '1, 4, 3'}).json())
-print(json.dumps(get('http://127.0.0.1:5000/api/jobs').json(), indent=2))
+get_all_users = get('http://127.0.0.1:5000/api/users')
+print(json.dumps(get_all_users.json(), indent=2))
+print('\n')
+get_user = get('http://127.0.0.1:5000/api/users/2')
+print(json.dumps(get_user.json(), indent=2))
+add_user = post('http://127.0.0.1:5000/api/users', json={'surname': 'AHHA',
+                                                         'name': 'KJHDSK',
+                                                         'age': 23,
+                                                         'position': 'ffffff',
+                                                         'speciality': 'klsdjfld',
+                                                         'address': 'lkdjlfjs',
+                                                         'email': 'herl@mail.ddj',
+                                                         'username': 'ushdhsjd',
+                                                         'password': 'herty'})
+print(add_user.json())
+edit_user = patch('http://127.0.0.1:5000/api/users/10', json={'surname': 'HHHHH', 'age': 50})
+print(edit_user.json())
